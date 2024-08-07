b71dc970c8567c0db5fcf11a3daf8937ab1f041c
thaovyvle <122627050+thaovyvle@users.noreply.github.com>
2024-08-07 15:25:28 -0400

    Some frontend

---
+++ app/chat.js
@@ -0,0 +1,241 @@
+'use client'
+
+import { Box, Button, Stack, TextField } from '@mui/material'
+import { useState, useRef, useEffect } from 'react'
+
+export default function Chat() {
+  const [messages, setMessages] = useState([
+    {
+      role: 'assistant',
+      content: "Hi! I'm PlantPal, your plant support assistant. How can I help you today?",
+    },
+  ])
+  const [message, setMessage] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const sendMessage = async () => {
+    console.log("Messages 1: ", message) // TODO: remove later
+
+    if (!message.trim() || isLoading) return;
+    setIsLoading(true)
+
+    setMessages((messages) => [
+      ...messages,
+      { role: 'user', content: message },
+      { role: 'assistant', content: '' },
+    ])
+    setMessage('')
+
+    console.log("Messages 2:", messages)
+  
+    try {
+      const response = await fetch('/api/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({messages: [...messages, {role: 'user', content: message}]}),
+      })
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+  
+      const reader = response.body.getReader()
+      const decoder = new TextDecoder()
+  
+      while (true) {
+        const { done, value } = await reader.read()
+        if (done) break
+        const text = decoder.decode(value, { stream: true })
+        console.log(text)
+        setMessages((messages) => {
+          let lastMessage = messages[messages.length - 1]
+          let otherMessages = messages.slice(0, messages.length - 1)
+          return [
+            ...otherMessages,
+            { ...lastMessage, content: lastMessage.content + text },
+          ]
+        })
+      }
+    } catch (error) {
+      console.error('Error:', error)
+      setMessages((messages) => [
+        ...messages,
+        { role: 'assistant', content: "I'm sorry, but I encountered an error. Please try again later." },
+      ])
+    }
+    setIsLoading(false)
+  }
+
+
+  /* For OpenAI
+  const [messages, setMessages] = useState([
+    {
+      role: 'assistant',
+      content: "Hi! I'm BuddyBot, your support assistant. How can I help you today?",
+    },
+  ])
+  const [message, setMessage] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const sendMessage = async () => {
+    if (!message.trim() || isLoading) return;
+    setIsLoading(true)
+  
+    setMessage('')
+    setMessages((messages) => [
+      ...messages,
+      { role: 'user', content: message },
+      { role: 'assistant', content: '' },
+    ])
+  
+    try {
+      const response = await fetch('/api/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({messages: messages}),
+      })
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+  
+      const reader = response.body.getReader()
+      const decoder = new TextDecoder()
+  
+      while (true) {
+        const { done, value } = await reader.read()
+        if (done) break
+        const text = decoder.decode(value, { stream: true })
+        setMessages((messages) => {
+          let lastMessage = messages[messages.length - 1]
+          let otherMessages = messages.slice(0, messages.length - 1)
+          return [
+            ...otherMessages,
+            { ...lastMessage, content: lastMessage.content + text },
+          ]
+        })
+      }
+    } catch (error) {
+      console.error('Error:', error)
+      setMessages((messages) => [
+        ...messages,
+        { role: 'assistant', content: "I'm sorry, but I encountered an error. Please try again later." },
+      ])
+    }
+    setIsLoading(false)
+  }
+  */
+ 
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault()
+      sendMessage()
+    }
+  }
+  const messagesEndRef = useRef(null)
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+  }
+
+  useEffect(() => {
+    scrollToBottom()
+    console.log("Message 3: ", messages)
+  }, [messages])
+
+  return (
+    <Stack display="flex" justifyContent="center" alignItems="center" width="100vw" height="100vh" >
+    <Box
+      width="100%"
+      height="100%"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      backgroundColor='#222a29'
+    >
+      <Stack
+        direction="column"
+        width="70%"
+        height="100%"
+        padding="20px"
+        margin='20px'
+        backgroundColor='white'
+        borderRadius={10}
+        spacing={2}
+        overflow="hidden"
+      >
+        <Stack
+          direction={'column'}
+          spacing={2}
+          flexGrow={1}
+          overflow="auto"
+          padding={1}
+        >
+          {messages.map((message, index) => (
+            <Box
+              key={index}
+              display="flex"
+              justifyContent={
+                message.role === 'assistant' ? 'flex-start' : 'flex-end'
+              }
+            >
+              <Box
+                bgcolor={
+                  message.role === 'assistant'
+                    ? '#86ba95'
+                    : '#06a177'
+                }
+                color="white"
+                borderRadius={message.role === 'assistant' 
+                    ? '16px 16px 16px 0px' 
+                    : '16px 16px 0px 16px'}
+                p={3}
+              >
+                {message.content}
+              </Box>
+            </Box>
+          ))}
+          <div ref={messagesEndRef} />
+        </Stack>
+        <Stack direction={'row'} spacing={2}>
+          <TextField
+            label="Message"
+            fullWidth
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            disabled={isLoading}
+            sx={{
+                borderRadius: '30px', 
+                '& .MuiOutlinedInput-root': {
+                  borderRadius: '30px',
+                  '&:hover fieldset': {
+                    borderColor: '#048c66', 
+                  },
+                  '&.Mui-focused fieldset': {
+                    borderColor: '#048c66',
+
+                  },
+                },
+              }}
+          />
+          <Button variant="contained" onClick={sendMessage} disabled={isLoading}
+          sx={{
+            borderRadius: '15px',
+            backgroundColor: '#06a177', 
+            '&:hover': {
+              backgroundColor: '#048c66',
+            },
+          }}>
+            {isLoading ? 'Sending...' : 'Send'}
+          </Button>
+        </Stack>
+      </Stack>
+    </Box>
+    </Stack>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,215 +1,100 @@
-'use client'
+'use client';
 
-import { Box, Button, Stack, TextField } from '@mui/material'
-import { useState, useRef, useEffect } from 'react'
+import { Box, Button, IconButton, Stack, Typography } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useState } from 'react';
+import Chat from './chat';
 
 export default function Home() {
-  const [messages, setMessages] = useState([
-    {
-      role: 'assistant',
-      content: "Hi! I'm BuddyBot, your support assistant. How can I help you today?",
-    },
-  ])
-  const [message, setMessage] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
+  const [showChat, setShowChat] = useState(false);
 
-  const sendMessage = async () => {
-    console.log("Messages 1: ", message) // TODO: remove later
+  const handleGoToChat = () => {
+    setShowChat(true);
+  };
 
-    if (!message.trim() || isLoading) return;
-    setIsLoading(true)
-
-    setMessages((messages) => [
-      ...messages,
-      { role: 'user', content: message },
-      { role: 'assistant', content: '' },
-    ])
-    setMessage('')
-
-    console.log("Messages 2:", messages)
-  
-    try {
-      const response = await fetch('/api/chat', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({messages: [...messages, {role: 'user', content: message}]}),
-      })
-  
-      if (!response.ok) {
-        throw new Error('Network response was not ok')
-      }
-  
-      const reader = response.body.getReader()
-      const decoder = new TextDecoder()
-  
-      while (true) {
-        const { done, value } = await reader.read()
-        if (done) break
-        const text = decoder.decode(value, { stream: true })
-        console.log(text)
-        setMessages((messages) => {
-          let lastMessage = messages[messages.length - 1]
-          let otherMessages = messages.slice(0, messages.length - 1)
-          return [
-            ...otherMessages,
-            { ...lastMessage, content: lastMessage.content + text },
-          ]
-        })
-      }
-    } catch (error) {
-      console.error('Error:', error)
-      setMessages((messages) => [
-        ...messages,
-        { role: 'assistant', content: "I'm sorry, but I encountered an error. Please try again later." },
-      ])
-    }
-    setIsLoading(false)
-  }
-
-
-  /* For OpenAI
-  const [messages, setMessages] = useState([
-    {
-      role: 'assistant',
-      content: "Hi! I'm BuddyBot, your support assistant. How can I help you today?",
-    },
-  ])
-  const [message, setMessage] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
-
-  const sendMessage = async () => {
-    if (!message.trim() || isLoading) return;
-    setIsLoading(true)
-  
-    setMessage('')
-    setMessages((messages) => [
-      ...messages,
-      { role: 'user', content: message },
-      { role: 'assistant', content: '' },
-    ])
-  
-    try {
-      const response = await fetch('/api/chat', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({messages: messages}),
-      })
-  
-      if (!response.ok) {
-        throw new Error('Network response was not ok')
-      }
-  
-      const reader = response.body.getReader()
-      const decoder = new TextDecoder()
-  
-      while (true) {
-        const { done, value } = await reader.read()
-        if (done) break
-        const text = decoder.decode(value, { stream: true })
-        setMessages((messages) => {
-          let lastMessage = messages[messages.length - 1]
-          let otherMessages = messages.slice(0, messages.length - 1)
-          return [
-            ...otherMessages,
-            { ...lastMessage, content: lastMessage.content + text },
-          ]
-        })
-      }
-    } catch (error) {
-      console.error('Error:', error)
-      setMessages((messages) => [
-        ...messages,
-        { role: 'assistant', content: "I'm sorry, but I encountered an error. Please try again later." },
-      ])
-    }
-    setIsLoading(false)
-  }
-  */
- 
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter' && !event.shiftKey) {
-      event.preventDefault()
-      sendMessage()
-    }
-  }
-  const messagesEndRef = useRef(null)
-
-  const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
-  }
-
-  useEffect(() => {
-    scrollToBottom()
-    console.log("Message 3: ", messages)
-  }, [messages])
+  const handleGoBack = () => {
+    setShowChat(false);
+  };
 
   return (
-    <Stack display="flex" justifyContent="center" alignItems="center" width="100vw" height="100vh" >
-    <Box
-      width="100vw"
-      height="85vh"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Stack
-        direction={'column'}
-        width="500px"
-        height="700px"
-        border="1px solid black"
-        p={2}
-        spacing={3}
-      >
-        <Stack
-          direction={'column'}
-          spacing={2}
-          flexGrow={1}
-          overflow="auto"
-          maxHeight="100%"
+    <Stack direction="row" justifyContent="center" alignItems="center" width="100vw" height="100vh" >
+      {showChat ? (
+        <Stack 
+          direction="column" 
+          justifyContent="center" 
+          alignItems="center" 
+          width="100%" 
+          height="100%" 
         >
-          {messages.map((message, index) => (
-            <Box
-              key={index}
-              display="flex"
-              justifyContent={
-                message.role === 'assistant' ? 'flex-start' : 'flex-end'
-              }
+          <Stack 
+            direction="row" 
+            alignItems="center" 
+            width="100%" 
+            paddingX={3}
+            backgroundColor={'#06a177'}
+          >
+            <IconButton 
+              onClick={handleGoBack} 
+              sx={{ color: 'white', padding: '10px', backgroundColor: '#9bd9ad' }}
             >
-              <Box
-                bgcolor={
-                  message.role === 'assistant'
-                    ? 'primary.main'
-                    : 'secondary.main'
-                }
-                color="white"
-                borderRadius={16}
-                p={3}
-              >
-                {message.content}
-              </Box>
-            </Box>
-          ))}
-          <div ref={messagesEndRef} />
+              <ArrowBackIcon />
+            </IconButton>
+            <Typography variant={'h5'} color={'white'} textAlign={'center'} fontWeight={"bold"} backgroundColor={'#06a177'} padding={'20px'} flexGrow={1} paddingLeft={'0px'}>
+              Chat with AI PlantPal
+            </Typography>
+          </Stack>
+          <Chat />
         </Stack>
-        <Stack direction={'row'} spacing={2}>
-          <TextField
-            label="Message"
-            fullWidth
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            disabled={isLoading}
+      ) : (
+        <>
+          <Box display="flex" flexDirection="column" alignItems="flex-start" paddingLeft="20px" width='50vw'>
+            <Typography variant={'h1'} color={'#06a177'} textAlign={'left'} fontWeight={"bold"} paddingBottom={'25px'}>
+              PlantPal
+            </Typography>
+            <Typography variant={'p'} color={'#304840'} textAlign={'left'} paddingBottom={'60px'} paddingRight={'60px'}>
+              Your AI plant assistant, offering expert advice on plant care and selection to help your plants flourish.
+            </Typography>
+            <Box
+              component="img"
+              src="/plants.png"
+              alt="PlantPal Avatar"
+              sx={{ maxWidth: '300px', width: '100%', height: 'auto', paddingBottom: '10px' }}
+            />
+            <Button 
+              variant="contained" 
+              color="primary" 
+              onClick={handleGoToChat}
+              sx={{ 
+                backgroundColor: '#9bd9ad', 
+                color: 'white', 
+                borderRadius: "30px",
+                width: '150px', 
+                height: '50px',
+                '&:hover': {
+                  backgroundColor: '#06a177',
+                },
+                marginTop: '10px' 
+              }}
+            >
+              Start Chat
+            </Button>
+          </Box>
+          <Box
+            component="img"
+            src={'/plantRobot.png'}
+            alt="AI Robot"
+            sx={{ 
+              maxWidth: '400px', 
+              height: 'auto', 
+              backgroundColor: '#fff9d4', 
+              borderRadius: '50px', 
+              padding: '20px', 
+              boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
+              display: showChat ? 'none' : 'block' // Hide the image when on chat page
+            }}
           />
-          <Button variant="contained" onClick={sendMessage} disabled={isLoading}>
-            {isLoading ? 'Sending...' : 'Send'}
-          </Button>
-        </Stack>
-      </Stack>
-    </Box>
+        </>
+      )}
     </Stack>
-  )
+  );
 }
